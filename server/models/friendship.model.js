f3b9e826056a6aae04539c31cbc4cd3f013cc467
Guillermo Concepción Hernández <44781085+BitterBaitBite@users.noreply.github.com>
2022-09-20 18:19:34 +0200

    Add files via upload

---
+++ server/models/friendship.model.js
@@ -0,0 +1,31 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = require('../db');
+const User = require('./user.model');
+
+const Friendship = sequelize.define(
+	'friendship',
+	{
+		// user1_id: {
+		// 	type: Sequelize.INTEGER,
+		// 	allowNull: false,
+		// 	references: {
+		// 		model: User,
+		// 		key: 'id',
+		// 	},
+		// },
+		// user2_id: {
+		// 	type: Sequelize.INTEGER,
+		// 	allowNull: false,
+		// 	references: {
+		// 		model: User,
+		// 		key: 'id',
+		// 	},
+		// },
+	},
+	{
+		timestamps: false,
+	}
+);
+
+module.exports = Friendship;
